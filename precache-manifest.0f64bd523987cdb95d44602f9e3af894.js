6d7e15ca779e7244e2310402b42501e5a5351724
shamitha <shamithasabdar@gmail.com>
2024-04-27 13:44:27 +0530

    Updates

---
+++ precache-manifest.0f64bd523987cdb95d44602f9e3af894.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24fdc5ed94850a36ab4a6de51fb12580",
+    "url": "/Trippy_ReactJS/index.html"
+  },
+  {
+    "revision": "8f42084637736873dc87",
+    "url": "/Trippy_ReactJS/static/css/main.61cf77e3.chunk.css"
+  },
+  {
+    "revision": "d591022129633c318bd5",
+    "url": "/Trippy_ReactJS/static/js/2.8b3d2570.chunk.js"
+  },
+  {
+    "revision": "b14cf38e36b679bf3c7c",
+    "url": "/Trippy_ReactJS/static/js/3.bb0829b5.chunk.js"
+  },
+  {
+    "revision": "8f42084637736873dc87",
+    "url": "/Trippy_ReactJS/static/js/main.d86ec917.chunk.js"
+  },
+  {
+    "revision": "2897ed800bc91c8e0bbb",
+    "url": "/Trippy_ReactJS/static/js/runtime~main.aec96157.js"
+  },
+  {
+    "revision": "cae72840781a4bf46b049dc83ec6ae4f",
+    "url": "/Trippy_ReactJS/static/media/1.cae72840.jpg"
+  },
+  {
+    "revision": "d0a05fc3e984e79b7a5c6e9124a210fd",
+    "url": "/Trippy_ReactJS/static/media/12.d0a05fc3.jpg"
+  },
+  {
+    "revision": "7cb3d913ca6197cfa9e6d7d197588335",
+    "url": "/Trippy_ReactJS/static/media/2.7cb3d913.jpg"
+  },
+  {
+    "revision": "3706dacadcef406a6483c69cc4aff5f8",
+    "url": "/Trippy_ReactJS/static/media/4.3706daca.jpg"
+  },
+  {
+    "revision": "44c1ae72a84350ca6997560601185451",
+    "url": "/Trippy_ReactJS/static/media/5.44c1ae72.jpg"
+  },
+  {
+    "revision": "74fce4d5d8cad3c0ba03fc6e3f30e7b2",
+    "url": "/Trippy_ReactJS/static/media/6.74fce4d5.jpg"
+  },
+  {
+    "revision": "cf7f2b0640dfe908b8cde51c72a65cf2",
+    "url": "/Trippy_ReactJS/static/media/8.cf7f2b06.jpg"
+  },
+  {
+    "revision": "cc3ac5f8f5deccd2cedc87182220947e",
+    "url": "/Trippy_ReactJS/static/media/night.cc3ac5f8.jpg"
+  }
+]);
